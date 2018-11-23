6c8796c53a6a45953880b9a895c7808f82ea7dda
kateszy <szalskakatarzyna@gmail.com>
2018-11-23 18:38:44 +0100

    testy02

---
+++ Stos/Testy/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testy")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("999f6889-1fb1-4eaa-8f1c-59326b20de9c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Stos/Testy/UnitTest1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+using Stos;
+
+namespace Testy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CanCreateInstanceOfStack()
+        {
+            new Stos.Stack<int>();
+
+        }
+
+        [TestMethod]
+        public void CanPushNotNullElementToStack()
+        {
+            Stos.Stack<int> stack = new Stos.Stack<int>();
+            stack.Push(1);
+        }
+
+        public void PushNullElementRaiseArgumentNullException()
+        {
+            Stos.Stack<int?> stack = new Stos.Stack<int?>();
+            Assert.ThrowsException<ArgumentNullException>(() => stack.Push(null));
+        }
+
+        [TestMethod]
+        public void CanPopElementFromNotEmptyList()
+        {
+            Stos.Stack<int> stack = new Stos.Stack<int>();
+            stack.Push(1);
+            Assert.AreEqual(stack.Pop(), 1);
+        }
+
+        [TestMethod]
+        public void PopReturnsSameObjectAsHasBeenPush()
+        {
+            List<int> element = new List<int>();
+            Stos.Stack<List<int>> stack = new Stos.Stack<List<int>>();
+            stack.Push(element);
+            Assert.AreSame(stack.Pop(), element);
+        }
+
+        [TestMethod]
+        public void PopEmptyStackaRaiseInvalidOperationException()
+        {
+            Stos.Stack<int> stack = new Stos.Stack<int>();
+            Assert.ThrowsException<InvalidOperationException>(() => stack.Pop());
+        }
+
+        [TestMethod]
+        public void StackPopAndPushWorksLikeLIFO()
+        {
+            Stos.Stack<int> stack = new Stos.Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Assert.AreEqual(stack.Pop(), 3);
+            Assert.AreEqual(stack.Pop(), 2);
+
+            stack.Push(4);
+            stack.Push(5);
+            Assert.AreEqual(stack.Pop(), 5);
+            Assert.AreEqual(stack.Pop(), 4);
+            Assert.AreEqual(stack.Pop(), 1);
+        }
+
+        [TestMethod]
+        public void PeekDosentRemoveObjectFromStackMemory()
+        {
+            Stos.Stack<int> stack = new Stos.Stack<int>();
+            stack.Push(1);
+            Assert.AreEqual(stack.Peek(), 1);
+            Assert.AreEqual(stack.Peek(), 1);
+        }
+
+        [TestMethod]
+        public void PeekEmptyStackaRaiseInvalidOperationException()
+        {
+            Stos.Stack<int> stack = new Stos.Stack<int>();
+            Assert.ThrowsException<InvalidOperationException>(() => stack.Peek());
+        }
+
+    }
+}
